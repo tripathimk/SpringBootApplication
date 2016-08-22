1c843658cbafdb54a14833d5de4cd7fe90c910ef
manojkyadav <manoj.dims@gmail.com>
2016-08-22 12:14:30 +1000

    testing
    testing

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -12,7 +12,7 @@ public class SampleController {
     @RequestMapping("/hello")
     @ResponseBody
     String home() {
-        return "Hello World! NEW  to branch RND";
+        return "Hello World! NEW  to branch RND Testing";
     }
 
     public static void main(String[] args) throws Exception {
